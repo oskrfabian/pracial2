c450dfa87d533ad7744ce397e9f0bf94a2b97184
oskrfabian <oskrfabian.system@gmail.com>
2018-04-13 23:01:38 -0600

    codigo de node +arduino

---
+++ parcial2_arduino/parcial2_arduino.ino
@@ -0,0 +1,24 @@
+/variable
+float sinVal;
+int toneVal;
+
+void setup(){
+     pinMode(8, OUTPUT); // Definimos el pin 8 como salida.
+                         
+     Serial.begin(9600);
+}
+// para zumador  
+void loop(){
+
+  
+  // el codigo la alarma 
+     for(int x=0; x<180; x++){
+      Serial.println("alarma ");
+            // convertimos grados en radianes para luego obtener el valor.
+            sinVal = (sin(x*(3.1412/180)));
+            // generar una frecuencia a partir del valor sin
+            toneVal = 2000+(int(sinVal*1000));
+            tone(8, toneVal);
+            delay(2); 
+     }   
+}
---
+++ parcial2_nodejs/index.js
@@ -0,0 +1,33 @@
+const TelegramBot = require('node-telegram-bot-api');
+ const token = '586106828:AAHNCn1kochGFaU25mysxF-LQR86cDL8Dek';
+var idChar = 548250795;
+ var SerialPort = require('serialport');
+ var port = new SerialPort('/dev/COM9', {
+   baudRate: 57600
+ });
+
+ const bot = new TelegramBot(token, {polling: true});
+
+ bot.on('message', (msg) => {
+   const chatId = msg.chat.id;
+   console.log("El id es " + chatId)
+   var respuesta = msg.text;
+   if (respuesta == "encender") {
+     console.log("encender alarma");
+     bot.sendMessage(chatId, 'Encendiendo zumbador');
+     miSerial.write("E");
+   } else if (respuesta == "apagar") {
+     console.log("apagar alarma");
+     bot.sendMessage(chatId, 'Apagando zumbador');
+     miSerial.write("A");
+   }
+ });
+ miSerial.setEncoding('utf8');
+
+miSerial.on('data', function(data) {
+  console.log('Data:', data);
+  if (data[0] == 'E') {
+    console.log("Boton precionado");
+    bot.sendMessage(idChar, "Precionador el boton");
+  }
+});
