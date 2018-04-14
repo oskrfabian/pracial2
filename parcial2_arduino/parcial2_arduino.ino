6e3a39860e3efa3b56f7f360f0f577e93f41f900
Bryan Velasquez <bryanvelasquez@Bryans-MacBook-Pro.local>
2018-04-14 07:01:19 -0600

    arduoino codigo

--- parcial2_arduino/parcial2_arduino.ino
+++ parcial2_arduino/parcial2_arduino.ino
@@ -1,24 +1,39 @@
-/variable
-float sinVal;
-int toneVal;
+//para el boton
+int boton = 2;
+//variable para el zumbador
+int zumbador = 6; //Pin PWM  a donde va conectado el zumbador
+
 
 void setup(){
-     pinMode(8, OUTPUT); // Definimos el pin 8 como salida.
-                         
+  pinMode(boton,INPUT_PULLUP);// para el boton
+  pinMode(zumbador, OUTPUT);// para el zumbador
+
      Serial.begin(9600);
 }
-// para zumador  
+// para zumador
 void loop(){
+// PARA EL BOTON
+if (digitalRead(boton)==HIGH){// if(digitalRead(boton)==o)
+  Serial.println("alarma");//imprime  "alarma"
+  digitalWrite (zumbador,HIGH);// ACTIVAR EL ZUMBADOR
+  delay (300); // el tiempo que esta activado el zumbador
+  digitalWrite(zumbador,LOW);
+      }
 
-  
-  // el codigo la alarma 
-     for(int x=0; x<180; x++){
-      Serial.println("alarma ");
-            // convertimos grados en radianes para luego obtener el valor.
-            sinVal = (sin(x*(3.1412/180)));
-            // generar una frecuencia a partir del valor sin
-            toneVal = 2000+(int(sinVal*1000));
-            tone(8, toneVal);
-            delay(2); 
-     }   
+//para el char
+
+  if (Serial.available() ) {
+    char letra = Serial.read();
+    if (letra == 'E') {//encendido
+      digitalWrite(zumbador, HIGH);
+    }
+    else if (letra == 'A') {//apagado
+      digitalWrite(zumbador, LOW);
+    }
+  }
+
+
+
+
+
 }
